af1a00b2e340b7711988384d6194aafe55cfb7eb
Henrik Korsgaard <korsgaard@cs.au.dk>
2016-01-07 09:56:28 -0700

    scaffolding tests

---
+++ tests/WebstrateClient.test.js
@@ -0,0 +1,34 @@
+'use strict';
+let assert = require( 'assert' );
+
+let WebstrateClient = require('../lib/WebstrateClient.js');
+
+describe( 'Testing WebstrateClient connectivity', function () {
+
+    describe( 'Initiation', function () {
+
+    } );
+
+    describe( 'Reload', function () {
+
+    } );
+
+    describe( 'Disconnect', function () {
+
+    } );
+} );
+
+describe( 'Testing webstrate.document', function () {
+
+    describe( 'Read', function () {
+
+    } );
+
+    describe( 'Change', function () {
+
+    } );
+
+    describe( 'MutationListener', function () {
+
+    } );
+} );
